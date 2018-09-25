4b22750df95e87755edca0b8598221cd50405a8a
gansiqiu <1223720954@qq.com>
2018-09-25 15:56:31 +0800

    fix

--- pages/Myinfo/Myinfo.js
+++ pages/Myinfo/Myinfo.js
@@ -51,7 +51,7 @@ Page({
     * 生命周期函数--监听页面加载
     */
    onLoad: function(options) {
-
+       
    },
 
    /**
@@ -65,7 +65,7 @@ Page({
     * 生命周期函数--监听页面显示
     */
    onShow: function() {
-
+       
    },
 
    /**
--- pages/curriculum/curriculum.js
+++ pages/curriculum/curriculum.js
@@ -1,103 +1,114 @@
 // pages/curriculum/curriculum.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-     currentTab:"0",
-     dataList:[],
-     autoHeight: 2400
-  },
-   handleChange:function(event){
-      this.setData({
-         currentTab: event.detail.key
-      });
-      if (event.detail.key == "0"){
-         this.setData({
-            autoHeight: 2400
-         });
-      }else{
-         this.setData({
-             autoHeight: 1020
-         });
-       }
-   },
-   bindchange:function(event){
-      this.setData({
-         currentTab: event.detail.current
-      });
-      if (event.detail.current == "0"){
-         this.setData({
-            autoHeight: 2400
-         });
-      }else{
-         this.setData({
-            autoHeight: 1020
-         });
-      }
-   },
-   catchtap:function(event){
-      wx.navigateTo({
-         url: '/pages/curriculum/curriculumList/curriculumList?classId=' + event.currentTarget.dataset.classId +
-               "&classTitle=" + event.currentTarget.dataset.classTitle,
-      })
-   },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        currentTab: "0",
+        dataList: [],
+        autoHeight: 2400
+    },
+    handleChange: function(event) {
+        this.setData({
+            currentTab: event.detail.key
+        });
+        if (event.detail.key == "0") {
+            this.setData({
+                autoHeight: 2400
+            });
+        } else {
+            this.setData({
+                autoHeight: 1020
+            });
+        }
+    },
+    bindchange: function(event) {
+        this.setData({
+            currentTab: event.detail.current
+        });
+        if (event.detail.current == "0") {
+            this.setData({
+                autoHeight: 2400
+            });
+        } else {
+            this.setData({
+                autoHeight: 1020
+            });
+        }
+    },
+    catchtap: function(event) {
+        wx.navigateTo({
+            url: '/pages/curriculum/curriculumList/curriculumList?classId=' + event.currentTarget.dataset.classId +
+                "&classTitle=" + event.currentTarget.dataset.classTitle,
+        })
+    },
 
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function(res) {
+        return {
+            title: '心一教育',
+            path: '/pages/curriculum/curriculum',
+            success: function (res) {
+                // 转发成功
+                // console.log("转发成功",res);
+            },
+            fail: function (res) {
+                // 转发失败
+                // console.log("转发失败", res);
+            }
+        }
+    }
 })
